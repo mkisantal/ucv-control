34ad67fe2c4099e3e3a02cf33a26261fd79041ba
mkisantal <kisantal.mate@gmail.com>
2017-08-07 14:38:12 +0200

    debugging A3C implementation

--- network.py
+++ network.py
@@ -30,73 +30,73 @@ def discount(x, gamma):
 
 class ACNetwork:
     def __init__(self, action_space_size, state_space_size, scope, trainer, ):
+        with tf.variable_scope(scope):
+            self.inputs = tf.placeholder(shape=[None]+state_space_size, dtype=tf.float32)  # grayscale or RGB?
+            self.conv1 = slim.conv2d(inputs=self.inputs,
+                                     num_outputs=16,
+                                     kernel_size=[8, 8],
+                                     stride=[4, 4],
+                                     padding='VALID',
+                                     activation_fn=tf.nn.elu)
+            self.conv2 = slim.conv2d(inputs=self.conv1,
+                                     num_outputs=32,
+                                     kernel_size=[4, 4],
+                                     stride=[2, 2],
+                                     padding='VALID',
+                                     activation_fn=tf.nn.elu)
+            hidden = slim.fully_connected(slim.flatten(self.conv2), 256, activation_fn=tf.nn.elu)
 
-        self.inputs = tf.placeholder(shape=[None]+state_space_size, dtype=tf.float32)  # grayscale or RGB?
-        self.conv1 = slim.conv2d(inputs=self.inputs,
-                                 num_outputs=16,
-                                 kernel_size=[8, 8],
-                                 stride=[4, 4],
-                                 padding='VALID',
-                                 activation_fn=tf.nn.elu)
-        self.conv2 = slim.conv2d(inputs=self.conv2,
-                                 num_outputs=32,
-                                 kernel_size=[4, 4],
-                                 stride=[2, 2],
-                                 padding='VALID',
-                                 activation_fn=tf.nn.elu)
-        hidden = slim.fully_connected(slim.flatten(self.conv2), 256, activation_fn=tf.nn.elu)
+            # LSTM
+            lstm_cell = rnn.BasicLSTMCell(256, state_is_tuple=True)
+            c_init = np.zeros((1, lstm_cell.state_size.c), np.float32)
+            h_init = np.zeros((1, lstm_cell.state_size.h), np.float32)
+            self.state_init = [c_init, h_init]
+            c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
+            h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
+            # self.state_in = (c_in, h_in)
+            rnn_in = tf.expand_dims(hidden, [0])
+            step_size = tf.shape(self.inputs)[:1]
+            self.state_in = rnn.LSTMStateTuple(c_in, h_in)
+            lstm_outputs, lstm_state = \
+                tf.nn.dynamic_rnn(lstm_cell,
+                                  rnn_in,
+                                  sequence_length=step_size,
+                                  initial_state=self.state_in,
+                                  time_major=False)
+            lstm_c, lstm_h = lstm_state
+            self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
+            rnn_out = tf.reshape(lstm_outputs, [-1, 256])
 
-        # LSTM
-        lstm_cell = rnn.BasicLSTMCell(256, state_is_tuple=True)
-        c_init = np.zeros((1, lstm_cell.state_size.c), tf.float32)
-        h_init = np.zeros((1, lstm_cell.state_size.h), tf.float32)
-        self.state_init = [c_init, h_init]
-        c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
-        h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
-        # self.state_in = (c_in, h_in)
-        rnn_in = tf.expand_dims(hidden, [0])
-        step_size = tf.shape(self.inputs)[:1]
-        self.state_in = rnn.LSTMStateTuple(c_in, h_in)
-        lstm_outputs, lstm_state = \
-            tf.nn.dynamic_rnn(lstm_cell,
-                              rnn_in,
-                              sequence_length=step_size,
-                              initial_state=self.state_in,
-                              time_major=False)
-        lstm_c, lstm_h = lstm_state
-        self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
-        rnn_out = tf.reshape(lstm_outputs, [-1, 256])
+            self.policy = slim.fully_connected(rnn_out, action_space_size,
+                                               activation_fn=tf.nn.softmax,
+                                               weights_initializer=normalized_columns_initializer(0.01),
+                                               biases_initializer=None)
+            self.value = slim.fully_connected(rnn_out, 1,
+                                              activation_fn=None,
+                                              weights_initializer=normalized_columns_initializer(1.0),
+                                              biases_initializer=None
+                                              )
 
-        self.policy = slim.fully_connected(rnn_out, action_space_size,
-                                           activation_fn=tf.nn.softmax,
-                                           weights_initializer=normalized_columns_initializer(0.01),
-                                           biases_initializer=None)
-        self.value = slim.fully_connected(rnn_out, 1,
-                                          activation_fn=None,
-                                          weights_initializer=normalized_columns_initializer(1.0),
-                                          biases_initializer=None
-                                          )
+            if scope != 'global':
+                self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
+                self.actions_onehot = tf.one_hot(self.actions, action_space_size, dtype=tf.float32)
+                self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
+                self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
 
-        if scope != 'global':
-            self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
-            self.actions_onehot = tf.one_hot(self.actions, action_space_size, dtype=tf.float32)
-            self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
-            self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
+                self.responsible_outputs = tf.reduce_sum(self.policy * self.actions_onehot, [1])
 
-            self.responsible_outputs = tf.reduce_sum(self.policy * self.actions_onehot, [1])
+                self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value, [-1])))
+                self.entropy = -tf.reduce_sum(self.policy * tf.log(self.policy))
+                self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs) * self.advantages)
+                self.loss = 0.5 * self.value_loss + self.policy_loss - self.entropy * 0.01
 
-            self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value, [-1])))
-            self.entropy = -tf.reduce_sum(self.policy * tf.log(self.policy))
-            self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs) * self.advantages)
-            self.loss = 0.5 * self.value_loss + self.policy_loss - self.entropy * 0.01
+                local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
+                self.gradients = tf.gradients(self.loss, local_vars)
+                self.var_norms = tf.global_norm(local_vars)
+                grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, 40.0)
 
-            local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
-            self.gradients = tf.gradients(self.loss, local_vars)
-            self.var_norms = tf.global_norm(local_vars)
-            grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, 40.0)
-
-            global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
-            self.apply_grads = trainer.apply_gradients(zip(grads, global_vars))
+                global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
+                self.apply_grads = trainer.apply_gradients(zip(grads, global_vars))
 
 
 class Worker:
@@ -112,7 +112,7 @@ class Worker:
         self.episode_mean_values = []
         self.summary_writer = tf.summary.FileWriter('train' + str(self.number))
 
-        self.local_AC = ACNetwork(self.name, trainer)
+        self.local_AC = ACNetwork(len(game.action_space), game.state_space_size, self.name, trainer)
         self.update_local_ops = update_target_graph('global', self.name)
 
         # setting up game here
@@ -127,15 +127,16 @@ class Worker:
         next_observations = rollout[:, 3]
         values = rollout[:, 5]
 
-        self.rewards_plus = np.asarray(rewards.tolist() + [bootstrap_value])
-        discounted_rewards = discount(self.rewards_plus, gamma)[:-1]
-        self.value_plus = np.asarray(values.tolist() + [bootstrap_value])
-        advantages = rewards + gamma * self.value_plus[1:] - self.value_plus[:-1]   # GAE?
+        rewards_plus = np.asarray(rewards.tolist() + [bootstrap_value])
+        discounted_rewards = discount(rewards_plus, gamma)[:-1]
+        value_plus = np.asarray(values.tolist() + [bootstrap_value])
+        advantages = rewards + gamma * value_plus[1:] - value_plus[:-1]   # GAE?
         advantages = discount(advantages, gamma)
 
         rnn_state = self.local_AC.state_init
         feed_dict = {self.local_AC.target_v: discounted_rewards,
-                     self.local_AC.inputs: np.vstack(observations),
+                     # self.local_AC.inputs: np.vstack(observations),
+                     self.local_AC.inputs: np.vstack(np.expand_dims(obs, 0) for obs in observations),
                      self.local_AC.actions: actions,
                      self.local_AC.advantages: advantages,
                      self.local_AC.state_in[0]: rnn_state[0],
@@ -164,7 +165,7 @@ class Worker:
                 d = False
 
                 self.env.new_episode()
-                s = self.env.get_state().screen_buffer
+                s = self.env.get_observation()
                 episode_frames.append(s)
                 # s = process_frame(s)
                 rnn_state = self.local_AC.state_init
@@ -182,7 +183,7 @@ class Worker:
                     r = self.env.action(self.actions[a])
                     d = self.env.is_episode_finished()
                     if d is False:
-                        s1 = self.env.get_state().screen_buffer
+                        s1 = self.env.get_observation()
                         episode_frames.append(s1)
                         # s1 = process_frame(s1)
                     else:
@@ -201,7 +202,7 @@ class Worker:
                                       feed_dict={self.local_AC.inputs: [s],
                                                  self.local_AC.state_in[0]: rnn_state[0],
                                                  self.local_AC.state_in[1]: rnn_state[1]})
-                        v_l, p_l, e_l, g_n, v_n = self.train(episode_buffer, sess, gamma, v1)
+                        v_l, p_l, e_l, g_n, v_n = self.train(episode_buffer, v1, gamma, sess)
                         episode_buffer = []
                         sess.run(self.update_local_ops)
                     if d:
@@ -212,7 +213,7 @@ class Worker:
                 self.episode_mean_values.append(np.mean(episode_values))
 
                 if len(episode_buffer) != 0:
-                    v_l, p_l, e_l, g_n, v_n = self.train(episode_buffer, 0.0, sess, gamma)
+                    v_l, p_l, e_l, g_n, v_n = self.train(episode_buffer, 0.0, gamma, sess)
 
                 if episode_count % 5 == 0 and episode_step_count != 0:
                     if episode_count % 250 == 0 and self.name == 'worker_0':
@@ -236,6 +237,8 @@ class Worker:
                     self.summary_writer.flush()
                 if self.name == 'worker_0':
                     sess.run(self.increment)
+                    if self.global_episodes > 10:   # TODO: max global episodes to hyperparameter
+                        coord.request_stop()
                 episode_count += 1
 
 
