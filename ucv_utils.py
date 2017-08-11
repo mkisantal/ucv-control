c258e8e124eacb82feca3c0b2b8cc4f0406452b2
mkisantal <kisantal.mate@gmail.com>
2017-08-11 15:16:35 +0200

    check resolution

--- ucv_utils.py
+++ ucv_utils.py
@@ -9,6 +9,10 @@ def set_port(port, sim_dir):
     for line in fileinput.FileInput(sim_dir + 'unrealcv.ini', inplace=1):
         if "Port=" in line:
             line = re.sub('\d{4}', str(port), line)
+        if "Width=" in line:
+            line = re.sub('\d{3}', str(84), line)
+        if "Height=" in line:
+            line = re.sub('\d{3}', str(84), line)
         print line,
 
 
