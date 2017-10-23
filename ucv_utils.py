5f34b4e4cda865f77e40410cb35cf613181b3df8
mkisantal <kisantal.mate@gmail.com>
2017-10-23 14:02:49 +0200

    create ini file if needed

--- ucv_utils.py
+++ ucv_utils.py
@@ -1,12 +1,25 @@
 import fileinput
 import re
+import os
+from subprocess import Popen
+from config import Config
+from time import sleep
+
 
 def set_port(port, sim_dir):
-    for line in fileinput.FileInput(sim_dir + 'unrealcv.ini', inplace=1):
-        if "Port=" in line:
-            line = re.sub('\d{4}', str(port), line)
-        if "Width=" in line:
-            line = re.sub('\d{2,4}', str(84), line)
-        if "Height=" in line:
-            line = re.sub('\d{2,4}', str(84), line)
-        print line,
+    try:
+        for line in fileinput.FileInput(sim_dir + 'unrealcv.ini', inplace=1):
+            if "Port=" in line:
+                line = re.sub('\d{4}', str(port), line)
+            if "Width=" in line:
+                line = re.sub('\d{2,4}', str(84), line)
+            if "Height=" in line:
+                line = re.sub('\d{2,4}', str(84), line)
+            print line,
+    except OSError:
+        print('unrealcv.ini does not exist, launching Sim to create it')
+        with open(os.devnull, 'w') as fp:
+            sim = Popen(Config.SIM_DIR + Config.SIM_NAME, stdout=fp)
+            sleep(5)
+            sim.terminate()
+            set_port(port, sim_dir)
